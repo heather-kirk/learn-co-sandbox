99bb60d7d2870490aa7c9d6858d9bdbd3ef4e2d9
Heather Kirk <hskirk@gmail.com>
2019-03-12 00:18:57 +0000

    updates

--- travel_cli/lib/top_travel/scraper.rb
+++ travel_cli/lib/top_travel/scraper.rb
@@ -1,11 +1,18 @@
+require_relative "./version.rb"
+
 class Travel::Scraper
   
   def scrape_locations
-  @doc = Nokogiri::HTML(open("https://www.cntraveller.com/gallery/top-holiday-destinations-2019"))
-  @doc.search("figcaption").each do |destination|
+  doc = Nokogiri::HTML(open("https://www.cntraveller.com/gallery/top-holiday-destinations-2019"))
+  div_titles = doc.css("div.c-figure__title").text.split.each{|l| l.capitalize!}
+  h2_titles = doc.css("h2.bb-h2").text.split.each{|l| l.capitalize!}
+  titles = div_titles + h2_titles 
+  binding.pry
+  doc.search("figcaption").each do |destination|
     location = Travel::Location.new
     
-     location.name = destination.search("h2.bb-h2").text.split.each{|l| l.capitalize!}.join(" ")
+    
+  
      location.subtitle = destination.search("h3.bb-h3").text
      location.summary = destination.search("p.bb-p").text
      location.booking = destination.search("strong.bb-strong").text
@@ -14,3 +21,5 @@ class Travel::Scraper
     end 
   end 
 end 
+
+
